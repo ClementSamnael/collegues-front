d7e3e0fde24505a01c8ec525745563d8ee7b9eb0
Clement ORMAUX <clement.ormaux@gmail.com>
2019-07-15 17:30:56 +0200

    Correction + image nulle

--- src/app/mock/Collegue.mock.ts
+++ src/app/mock/Collegue.mock.ts
@@ -1,7 +1,7 @@
 import {Collegue} from '../model/Collegue'
 
 let collegue:Collegue = new Collegue('913ff890-992d-4e49-bac0-a385704afb83', 'LEROY', 'Yoann', 'yoannleroy@mail.fr', new Date(1988,11,2), 
-'https://upload.wikimedia.org/wikipedia/commons/0/09/Man_Silhouette.png');
+'https://vignette.wikia.nocookie.net/jojo/images/6/6a/Star_Platinum_%28Stardust_Crusaders%2C_manga%29.png/revision/latest?cb=20180104204918&path-prefix=fr');
 
 export{
     collegue as unCollegue
